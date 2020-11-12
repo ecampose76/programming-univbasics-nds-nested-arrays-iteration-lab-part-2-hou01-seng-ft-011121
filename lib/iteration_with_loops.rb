4689adcd08ad0b6bf9848978df29ba56187cc45f
Elias Campos <ecampose76@gmail.com>
2020-11-12 20:43:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ while row_index < src.count do
   element_index = 0 
   min_element = 0 
   while element_index < src[row_index].count do 
-    if src[row_index][element_index].min < min_element.min
+    if src[row_index][element_index] < min_element
       min_element << src[row_index][element_index]
     end
     element_index += 1 
